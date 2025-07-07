0f6822c95fcc31a03f4c79edca7de2935dbbae8b
R. Sabbir <devsabbir37@gmail.com>
2025-07-07 11:38:25 +0000

    inrise tteh width of logo  200px and also  use  #0AB0FA and #0175CC  ada

--- src/components/layout/Logo.tsx
+++ src/components/layout/Logo.tsx
@@ -6,8 +6,8 @@ export function Logo({ className }: { className?: string }) {
     <Image
       src="https://6000-firebase-studio-1751870790151.cluster-ys234awlzbhwoxmkkse6qo3fz6.cloudworkstations.dev/capra/attachedImages/64d6697e-ee29-40b0-adaf-390271442b96/image_f1765b1c-8205-43a9-828b-5a30a615b6a5.png"
       alt="HomeCalc Pro Logo"
-      width={160}
-      height={27}
+      width={200}
+      height={34}
       className={cn(className)}
       priority
       unoptimized
