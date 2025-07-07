13eab4684c03d039c05445ec0c67132c7906e31b
R. Sabbir <devsabbir37@gmail.com>
2025-07-07 10:32:37 +0000

    use a main colors backound and  brder redius and make the icon wchite

--- src/components/layout/Logo.tsx
+++ src/components/layout/Logo.tsx
@@ -2,12 +2,14 @@ import Image from 'next/image';
 
 export function Logo() {
   return (
-    <Image
-      src="https://cdn-icons-png.flaticon.com/512/2825/2825684.png"
-      alt="HomeCalc Pro Logo"
-      width={32}
-      height={32}
-      className="h-8 w-8"
-    />
+    <div className="bg-primary rounded-md h-8 w-8 flex items-center justify-center">
+      <Image
+        src="https://cdn-icons-png.flaticon.com/512/2825/2825684.png"
+        alt="HomeCalc Pro Logo"
+        width={20}
+        height={20}
+        className="h-5 w-5 invert"
+      />
+    </div>
   );
 }
