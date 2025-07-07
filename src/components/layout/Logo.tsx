b58ce09db3e699e4a327ed3dde865ac35ae81743
R. Sabbir <devsabbir37@gmail.com>
2025-07-07 11:30:19 +0000

    https://6000-firebase-studio-1751870790151.cluster-ys234awlzbhwoxmkkse6q

--- src/components/layout/Logo.tsx
+++ src/components/layout/Logo.tsx
@@ -4,7 +4,7 @@ import Image from "next/image";
 export function Logo({ className }: { className?: string }) {
   return (
     <Image
-      src="https://storage.googleapis.com/aifirebase-dev-staging-1p-bucket/projects/project-883314144415/files/logo.png"
+      src="https://6000-firebase-studio-1751870790151.cluster-ys234awlzbhwoxmkkse6qo3fz6.cloudworkstations.dev/capra/attachedImages/64d6697e-ee29-40b0-adaf-390271442b96/image_f1765b1c-8205-43a9-828b-5a30a615b6a5.png"
       alt="HomeCalc Pro Logo"
       width={160}
       height={27}
