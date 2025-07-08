429b0fbe2c4ec77c91ca3d6a8a72e7ff5dab319b
R. Sabbir <devsabbir37@gmail.com>
2025-07-08 04:42:45 +0000

    not liked this

--- src/components/layout/Logo.tsx
+++ src/components/layout/Logo.tsx
@@ -9,7 +9,7 @@ export function Logo({ className }: { className?: string }) {
       <span
         className="inline-block rounded-md bg-primary px-2 py-1 text-lg font-bold leading-none text-primary-foreground font-headline"
         style={{
-          boxShadow: '0 4px 6px -1px rgb(0 0 0 / 0.1), 0 2px 4px -2px rgb(0 0 0 / 0.1), 0 0 20px 4px rgba(1, 149, 223, 0.4)',
+          boxShadow: '0 4px 12px rgba(1, 149, 223, 0.3)',
         }}
       >
         Pro
