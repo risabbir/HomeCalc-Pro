6386d21c1be5262c2dcb38ccae9be4aaeba83606
R. Sabbir <devsabbir37@gmail.com>
2025-07-08 17:19:37 +0000

    remove the hox shadoows frm logo pro

--- src/components/layout/Logo.tsx
+++ src/components/layout/Logo.tsx
@@ -8,9 +8,6 @@ export function Logo({ className }: { className?: string }) {
       </span>
       <span
         className="inline-block rounded-md bg-primary px-2 py-1 text-lg font-bold leading-none text-primary-foreground font-headline"
-        style={{
-          boxShadow: 'hsla(200, 99%, 44%, 0.4) 0px 5px 15px',
-        }}
       >
         Pro
       </span>
