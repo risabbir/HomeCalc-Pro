1ff251f206b2699a287cd1a6e769a4975c023d72
R. Sabbir <devsabbir37@gmail.com>
2025-07-08 04:42:16 +0000

    make teh logos pro shadows more better

--- src/components/layout/Logo.tsx
+++ src/components/layout/Logo.tsx
@@ -8,7 +8,9 @@ export function Logo({ className }: { className?: string }) {
       </span>
       <span
         className="inline-block rounded-md bg-primary px-2 py-1 text-lg font-bold leading-none text-primary-foreground font-headline"
-        style={{ boxShadow: '0 4px 14px 0 rgba(1, 149, 223, 0.39)' }}
+        style={{
+          boxShadow: '0 4px 6px -1px rgb(0 0 0 / 0.1), 0 2px 4px -2px rgb(0 0 0 / 0.1), 0 0 20px 4px rgba(1, 149, 223, 0.4)',
+        }}
       >
         Pro
       </span>
