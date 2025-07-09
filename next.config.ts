8310205ce716c5068a983344470c249cd6e5ddd3
R. Sabbir <devsabbir37@gmail.com>
2025-07-09 14:25:34 +0000

    when ihave copy image adress and paste in new tab shwoing this "url" par

--- next.config.ts
+++ next.config.ts
@@ -35,6 +35,12 @@ const nextConfig: NextConfig = {
         port: '',
         pathname: '/**',
       },
+      {
+        protocol: 'https',
+        hostname: 'www.researchgate.net',
+        port: '',
+        pathname: '/**',
+      },
     ],
   },
 };
--- src/app/resources/climate-zone-map/page.tsx
+++ src/app/resources/climate-zone-map/page.tsx
@@ -48,11 +48,11 @@ export default function ClimateZoneMapPage() {
                     <Card className="overflow-hidden border-2">
                              <CardContent className="p-0 bg-white">
                                 <Image
-                                    src="https://www.energy.gov/sites/default/files/2023-10/BSI_23_018447_US-climate-zone-map_v2_10-23-23-01.png"
+                                    src="https://www.researchgate.net/publication/359387671/figure/fig2/AS:1139834437545984@1648769115133/The-climate-zone-designations-used-by-the-US-Department-of-Energy-Building-America.png"
                                     data-ai-hint="climate zone map"
                                     alt="A map of the United States showing the 8 building climate zones as defined by the U.S. Department of Energy."
-                                    width={1200}
-                                    height={927}
+                                    width={850}
+                                    height={582}
                                     className="w-full h-auto object-contain"
                                     priority
                                 />
