a26ec2796855de9ca4437d813b9a3bd9c1908732
R. Sabbir <devsabbir37@gmail.com>
2025-07-10 11:19:22 +0000

    Make the following changes:
    ,
    
    Find this element:
    
      <AnimatedHamburger

--- src/components/layout/AnimatedHamburgerIcon.tsx
+++ src/components/layout/AnimatedHamburgerIcon.tsx
@@ -12,22 +12,22 @@ export function AnimatedHamburgerIcon({ open, className }: AnimatedHamburgerIcon
             <span
                 aria-hidden="true"
                 className={cn(
-                    "absolute block h-[2.5px] w-full transform bg-current transition duration-300 ease-in-out",
-                    open ? "rotate-45" : "-translate-y-2"
+                    "absolute block h-[2.5px] w-full transform bg-current transition duration-300 ease-in-out left-0",
+                    open ? "rotate-45 top-1/2 -translate-y-1/2" : "top-[6px]"
                 )}
             ></span>
             <span
                 aria-hidden="true"
                 className={cn(
-                    "absolute block h-[2.5px] w-full transform bg-current transition duration-300 ease-in-out",
+                    "absolute block h-[2.5px] w-full transform bg-current transition duration-300 ease-in-out left-0 top-1/2 -translate-y-1/2",
                     open ? "opacity-0" : ""
                 )}
             ></span>
             <span
                 aria-hidden="true"
                 className={cn(
-                    "absolute block h-[2.5px] w-full transform bg-current transition duration-300 ease-in-out",
-                    open ? "-rotate-45" : "translate-y-2"
+                    "absolute block h-[2.5px] w-full transform bg-current transition duration-300 ease-in-out left-0",
+                    open ? "-rotate-45 top-1/2 -translate-y-1/2" : "bottom-[6px]"
                 )}
             ></span>
         </div>
